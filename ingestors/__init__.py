0dacf459666c9319d00b00422d471982e4df8593
Simon Wörpel <simon.woerpel@pm.me>
2025-06-18 14:42:37 +0200

    🔊 Tweak global logging

--- ingestors/__init__.py
+++ ingestors/__init__.py
@@ -2,10 +2,17 @@
 
 import logging
 
+from anystore.logging import configure_logging, get_logger
+from procrastinate import cli as procrastinate_cli
+
 __version__ = "3.22.2"
+
+configure_logging()
 
 logging.getLogger("chardet").setLevel(logging.INFO)
 logging.getLogger("PIL").setLevel(logging.INFO)
 logging.getLogger("google.auth").setLevel(logging.INFO)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 logging.getLogger("msglite").setLevel(logging.WARNING)
+
+procrastinate_cli.print_stderr = get_logger("procrastinate.cli").info
