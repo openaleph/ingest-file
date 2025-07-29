3e9ccd38d68e3a1be463d53f5885c2dec0ad875b
Simon Wörpel <simon.woerpel@pm.me>
2025-07-29 12:58:31 +0200

    🐛 That was dumb

--- tests/test_tika.py
+++ tests/test_tika.py
@@ -4,11 +4,11 @@ from tests.support import TestCase
 
 class TikaIngestorTest(TestCase):
     def test_match(self):
-        fixture_path, entity = self.fixture("script.py")
+        fixture_path, entity = self.fixture("translate.po")
         assert self.manager.auction(fixture_path, entity) == TikaIngestor
 
     def test_ingest(self):
-        fixture_path, entity = self.fixture("script.py")
+        fixture_path, entity = self.fixture("translate.po")
         self.manager.ingest(fixture_path, entity)
         entity = self.get_emitted()[0]
-        assert entity.first("bodyText") == 'print("hello world")'
+        assert entity.first("bodyText").startswith("# Copyright")
