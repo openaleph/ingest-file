d0f6f04c77d8982195e800544d381a8f8c4d0a3e
Simon Wörpel <simon.woerpel@pm.me>
2025-07-29 08:51:12 +0200

    🔥 Drop unused settings

--- ingestors/settings.py
+++ ingestors/settings.py
@@ -25,14 +25,5 @@ class Settings(OpenAlephSettings):
     """Headless libreoffice document convert timeout in seconds"""
 
 
-settings = Settings()
-
 # Also store cached values in the SQL database
 sls.TAGS_DATABASE_URI = env.get("FTM_STORE_URI", env.get("ALEPH_DATABASE_URI"))
-
-# ProcessingException is thrown whenever something goes wrong with
-# parsing a file. Enable this with care, it can easily eat up the
-# Sentry quota of events.
-SENTRY_CAPTURE_PROCESSING_EXCEPTIONS = env.to_bool(
-    "SENTRY_CAPTURE_PROCESSING_EXCEPTIONS", False
-)
