1c72b9c9ce8167f7827bb75ec4ad2b39ffd554e9
Simon Wörpel <simon.woerpel@pm.me>
2025-07-29 07:48:51 +0200

    🐛 (support/email) Catch empty name

--- ingestors/support/email.py
+++ ingestors/support/email.py
@@ -19,9 +19,11 @@ class EmailIdentity(object):
     def __init__(self, manager, name, email):
         self.email = ascii_text(stringify(email))
         self.name = stringify(name)
+        if not self.name:
+            self.name = None
         if not registry.email.validate(self.email):
             self.email = None
-        if registry.email.validate(self.name):
+        if self.name and registry.email.validate(self.name):
             self.email = self.email or ascii_text(self.name)
             self.name = None
 
