5e03fbf799c09788e995a75447ba5ed13e0f9f9f
Simon Wörpel <simon.woerpel@pm.me>
2025-12-17 11:02:32 +0100

    🔖 Bump version: 5.1.0-rc11 → 5.1.0-rc12

--- ingestors/__init__.py
+++ ingestors/__init__.py
@@ -5,7 +5,7 @@ import logging
 from anystore.logging import configure_logging, get_logger
 from procrastinate import cli as procrastinate_cli
 
-__version__ = "5.1.0-rc11"
+__version__ = "5.1.0-rc12"
 
 configure_logging()
 
