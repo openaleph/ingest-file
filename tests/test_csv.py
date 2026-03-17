f0685600b96e2ac5b7cad6e45dcf82bc2febd1e9
jlstro <jstrozyk@gmail.com>
2026-03-17 14:22:37 +0100

    add fallback for wrongly guessed csv delims

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -10,6 +10,13 @@ class CSVIngestorTest(TestCase):
         self.assertTrue(entity.has("csvHash"))
         self.assertEqual(int(entity.first("rowCount")), 257)
 
+    def test_inconsistent_columns_csv(self):
+        fixture_path, entity = self.fixture("inconsistent_columns.csv")
+        self.manager.ingest(fixture_path, entity)
+        self.assertEqual(entity.first("processingStatus"), self.manager.STATUS_SUCCESS)
+        self.assertTrue(entity.has("csvHash"))
+        self.assertEqual(entity.first("columns"), '["Column 1"]')
+
     def test_nonutf_csv(self):
         fixture_path, entity = self.fixture("countries_nonutf.csv")
         self.manager.ingest(fixture_path, entity)
